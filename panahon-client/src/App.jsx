8ba80419e408496af847e55d063747ee4a0075fb
Marius <aceteaxh@gmail.com>
2026-03-11 13:24:41 +0800

    lab-act1

--- panahon-client/src/App.jsx
+++ panahon-client/src/App.jsx
@@ -4,32 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class Name="App">
+        <header className="App-header">
+          <h1>Welcome to my React App!</h1>
+          <p>
+            Name: Marius Clarence P. Panahon<br/>
+            Email: mariuspanahon@example.com<br/>
+            Other Personal Info: young stunna, 21 years old<br/>
+            <a href="https://github.com/M4r-shall/Panahon_webprog" target="github"></a>
+          </p>
+          </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
